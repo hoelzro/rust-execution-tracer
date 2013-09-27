9dd66ffe6133f5aaf0cbf26c259f32548f5c0c4f
Rob Hoelz <rob@hoelz.ro>
2013-09-27 18:04:47 +0200

    Add source that works with 0.6

---
+++ posix.rs
@@ -0,0 +1,191 @@
+extern mod c {
+    unsafe fn fork() -> libc::pid_t;
+    unsafe fn exit(status: libc::c_int) -> !;
+    unsafe fn getpid() -> libc::pid_t;
+    unsafe fn waitpid(pid: libc::pid_t, status: *libc::c_int, flags: libc::c_int) -> libc::c_int;
+    unsafe fn execvp(file: *libc::c_char, argv: **libc::c_char) -> !;
+    unsafe fn kill(pid: libc::pid_t, signal: libc::c_int) -> libc::c_int;
+    unsafe fn strerror(errno: libc::c_int) -> *libc::c_char;
+}
+
+pub trait CouldBeAnError {
+    fn is_error(&self) -> bool;
+    fn get_error_as_string(&self) -> ~str;
+    fn get_errno(&self) -> int;
+}
+
+pub enum PosixResult {
+    PosixOk,
+    PosixError(int),
+}
+
+pub fn strerror(errno: int) -> ~str {
+    unsafe {
+        str::raw::from_c_str(c::strerror(errno as libc::c_int))
+    }
+}
+
+impl CouldBeAnError for PosixResult {
+    fn is_error(&self) -> bool {
+        match *self {
+            PosixOk       => false,
+            PosixError(_) => true,
+        }
+    }
+
+    fn get_error_as_string(&self) -> ~str {
+        match *self {
+            PosixOk           => ~"no error",
+            PosixError(errno) => strerror(errno),
+        }
+    }
+
+    fn get_errno(&self) -> int {
+        match *self {
+            PosixOk           => fail!(~"You can't get an errno from a success value!"),
+            PosixError(errno) => errno,
+        }
+    }
+}
+
+pub enum ForkResult {
+    ForkFailure(int),
+    ForkChild,
+    ForkParent(int),
+}
+
+impl CouldBeAnError for ForkResult {
+    fn is_error(&self) -> bool {
+        match *self {
+            ForkFailure(_) => true,
+            _              => false,
+        }
+    }
+
+    fn get_error_as_string(&self) -> ~str {
+        match *self {
+            ForkFailure(errno) => strerror(errno),
+            _                  => ~"no error",
+        }
+    }
+
+    fn get_errno(&self) -> int {
+        match *self {
+            ForkFailure(errno) => errno,
+            _                  => fail!(~"You can't get an errno from a success value!"),
+        }
+    }
+}
+
+pub enum WaitPidResult {
+    WaitPidFailure(int),
+    WaitPidSuccess(int, int),
+}
+
+impl CouldBeAnError for WaitPidResult {
+    fn is_error(&self) -> bool {
+        match *self {
+            WaitPidFailure(_) => true,
+            _                 => false,
+        }
+    }
+
+    fn get_error_as_string(&self) -> ~str {
+        match *self {
+            WaitPidFailure(errno) => strerror(errno),
+            _                     => ~"no error",
+        }
+    }
+
+    fn get_errno(&self) -> int {
+        match *self {
+            WaitPidFailure(errno) => errno,
+            _                     => fail!(~"You can't get an errno from a success value!"),
+        }
+    }
+}
+
+pub fn fork() -> ForkResult {
+    unsafe {
+        let pid = c::fork();
+
+        match pid {
+            -1  => ForkFailure(os::errno()),
+            0   => ForkChild,
+            pid => ForkParent(pid as int),
+        }
+    }
+}
+
+pub fn getpid() -> int {
+    unsafe {
+        c::getpid() as int
+    }
+}
+
+pub fn waitpid(pid: int, flags: int) -> WaitPidResult {
+    unsafe {
+        let status : libc::c_int = 0;
+
+        let pid = c::waitpid(pid as libc::pid_t, &status as *libc::c_int, flags as libc::c_int);
+
+        if pid == -1 {
+            WaitPidFailure(os::errno())
+        } else {
+            WaitPidSuccess(pid as int, status as int)
+        }
+    }
+}
+
+// this is probably pretty awful...
+fn str_array_to_char_pp(ary: &[~str], callback: &fn(**libc::c_char)) {
+    fn helper_fn(ptrs: &mut ~[*libc::c_char], ary: &[~str], callback: &fn(**libc::c_char)) {
+        match ary {
+            [] => {
+                unsafe {
+                    ptrs.push(ptr::null());
+                    callback(vec::raw::to_ptr(*ptrs));
+                }
+            },
+            [head, ..tail] => {
+                do str::as_c_str(head) |raw_str| {
+                    ptrs.push(raw_str);
+                    helper_fn(ptrs, tail, callback);
+                }
+            },
+        }
+    }
+
+    let mut ptrs : ~[*libc::c_char] = vec::with_capacity(ary.len());
+
+    helper_fn(&mut ptrs, ary, callback);
+}
+
+pub fn exec(command_and_args: &[~str]) {
+    unsafe {
+        do str::as_c_str(command_and_args[0]) |command| {
+            do str_array_to_char_pp(command_and_args) |args| {
+                c::execvp(command, args);
+            }
+        }
+    }
+}
+
+pub fn exit(status: int) -> ! {
+    unsafe {
+        c::exit(status as libc::c_int)
+    }
+}
+
+pub fn kill(pid: int, signum: int) -> PosixResult {
+    unsafe {
+        match c::kill(pid as libc::pid_t, signum as libc::c_int) {
+            -1 => PosixError(os::errno()),
+            _  => PosixOk,
+        }
+    }
+}
+
+pub static SIGTRAP : int = 5;
+pub static SIGKILL : int = 9;
+pub static ECHILD  : int = 10;
---
+++ ptrace.rs
@@ -0,0 +1,171 @@
+use posix::CouldBeAnError;
+
+mod posix;
+
+extern mod c {
+    unsafe fn ptrace(request: libc::c_int, pid: libc::pid_t, addr: *libc::c_void, data: *libc::c_void) -> libc::c_long;
+}
+
+pub enum PtraceResult {
+    PtraceOk,
+    PtraceError(int),
+}
+
+impl CouldBeAnError for PtraceResult {
+    fn is_error(&self) -> bool {
+        match *self {
+            PtraceError(_) => true,
+            _              => false,
+        }
+    }
+
+    fn get_error_as_string(&self) -> ~str {
+        match *self {
+            PtraceError(errno) => posix::strerror(errno),
+            _                  => ~"no error",
+        }
+    }
+
+    fn get_errno(&self) -> int {
+        match *self {
+            PtraceError(errno) => errno,
+            _                  => fail!(~"You can't get an errno from a success value!"),
+        }
+    }
+}
+
+// XXX this type definition might not belong here
+pub type word = u64;
+
+// XXX extracting from headers would be nice
+static TRACEME    : libc::c_int = 0;
+static PEEKTEXT   : libc::c_int = 1;
+static GETREGS    : libc::c_int = 12;
+static SYSCALL    : libc::c_int = 24;
+static SETOPTIONS : libc::c_int = 0x4200;
+
+// XXX different on x86_64 vs x86
+pub struct UserRegs {
+  r15       : libc::uint64_t,
+  r14       : libc::uint64_t,
+  r13       : libc::uint64_t,
+  r12       : libc::uint64_t,
+  rbp       : libc::uint64_t,
+  rbx       : libc::uint64_t,
+  r11       : libc::uint64_t,
+  r10       : libc::uint64_t,
+  r9        : libc::uint64_t,
+  r8        : libc::uint64_t,
+  rax       : libc::uint64_t,
+  rcx       : libc::uint64_t,
+  rdx       : libc::uint64_t,
+  rsi       : libc::uint64_t,
+  rdi       : libc::uint64_t,
+  orig_rax  : libc::uint64_t,
+  rip       : libc::uint64_t,
+  cs        : libc::uint64_t,
+  eflags    : libc::uint64_t,
+  rsp       : libc::uint64_t,
+  ss        : libc::uint64_t,
+  fs_base   : libc::uint64_t,
+  gs_base   : libc::uint64_t,
+  ds        : libc::uint64_t,
+  es        : libc::uint64_t,
+  fs        : libc::uint64_t,
+  gs        : libc::uint64_t,
+}
+
+fn to_ptrace_result(return_value: libc::c_long) -> PtraceResult {
+    match return_value {
+        -1 => PtraceError(os::errno()),
+        _  => PtraceOk,
+    }
+}
+
+pub fn trace_me() -> PtraceResult {
+    unsafe {
+        to_ptrace_result(c::ptrace(TRACEME, 0, ptr::null(), ptr::null()))
+    }
+}
+
+pub fn setoptions(pid: int, options: int) -> PtraceResult {
+    unsafe {
+        to_ptrace_result(c::ptrace(SETOPTIONS, pid as libc::pid_t, ptr::null(), options as *libc::c_void))
+    }
+}
+
+pub fn syscall(pid: int) -> PtraceResult {
+    unsafe {
+        to_ptrace_result(c::ptrace(SYSCALL, pid as libc::pid_t, ptr::null(), ptr::null()))
+    }
+}
+
+// XXX this should probably return Result<~UserRegs, int>
+pub fn get_registers(pid: int) -> Result<UserRegs, int> {
+    unsafe {
+        // XXX is there a better way to do this?
+        let registers = UserRegs {
+          r15       : 0,
+          r14       : 0,
+          r13       : 0,
+          r12       : 0,
+          rbp       : 0,
+          rbx       : 0,
+          r11       : 0,
+          r10       : 0,
+          r9        : 0,
+          r8        : 0,
+          rax       : 0,
+          rcx       : 0,
+          rdx       : 0,
+          rsi       : 0,
+          rdi       : 0,
+          orig_rax  : 0,
+          rip       : 0,
+          cs        : 0,
+          eflags    : 0,
+          rsp       : 0,
+          ss        : 0,
+          fs_base   : 0,
+          gs_base   : 0,
+          ds        : 0,
+          es        : 0,
+          fs        : 0,
+          gs        : 0,
+        };
+
+        let result = c::ptrace(GETREGS, pid as libc::pid_t, ptr::null(), cast::transmute(&registers));
+
+        if result == -1 {
+            Err(os::errno())
+        } else {
+            Ok(registers)
+        }
+    }
+}
+
+pub fn peektext(pid: int, addr: *libc::c_void) -> Result<word, int> {
+    unsafe {
+        let result = c::ptrace(PEEKTEXT, pid as libc::pid_t, addr, ptr::null());
+
+        if result == -1 {
+            let errno = os::errno();
+
+            if errno != 0 {
+                Err(errno)
+            } else {
+                Ok(result as word)
+            }
+        } else {
+            Ok(result as word)
+        }
+    }
+}
+
+pub static TRACESYSGOOD : int = 0x00000001;
+pub static TRACEFORK    : int = 0x00000002;
+pub static TRACEEXEC    : int = 0x00000010;
+
+pub mod syscall {
+    pub static EXECVE : ::ptrace::word = 59;
+}
