b2a11384c714d82d2ee4d629854960c56bc0cdcf
Rob Hoelz <rob@hoelz.ro>
2014-09-28 17:54:21 -0500

    Use println! instead of stdio::println(format!(...))

--- trace-exec.rs
+++ trace-exec.rs
@@ -165,7 +165,7 @@ fn get_program_args(pid: int, addr: *libc::c_void) -> Vec<String> {
 
 fn handle_syscall_arguments(pid: int, (_, argv_ptr, _, _, _, _): (word, word, word, word, word, word)) {
     let argv = get_program_args(pid, argv_ptr as *libc::c_void);
-    stdio::println(format!("executable args: '{:?}'", argv.slice_from(0)));
+    println!("executable args: '{}'", argv);
 }
 
 fn run_parent(child_pid: int) -> TraceResult {
@@ -215,14 +215,14 @@ fn main() {
             posix::exit(255);
         }
         posix::ForkFailure(_) => {
-            stdio::println(format!("An error occurred: {}", result.get_error_as_string()));
+            println!("An error occurred: {}", result.get_error_as_string());
         }
         posix::ForkParent(child_pid) => {
             let result = run_parent(child_pid);
 
             if result.is_error() {
                 posix::kill(child_pid, posix::SIGKILL);
-                stdio::println(format!("An error occurred: {}", result.get_error_as_string()));
+                println!("An error occurred: {}", result.get_error_as_string());
             }
         }
     }
