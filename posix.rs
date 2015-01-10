4ec6bca791c0e66908400f52251b1beb3f93d7ea
Rob Hoelz <rob@hoelz.ro>
2015-01-10 16:10:58 -0600

    Got rid of boxed closures
    At least that's what I think they're called; the compiler complained
    saying I should use unboxed closures =/

--- posix.rs
+++ posix.rs
@@ -141,8 +141,8 @@ pub fn waitpid(pid: int, flags: int) -> WaitPidResult {
 }
 
 // this is probably pretty awful...
-fn str_array_to_char_pp(ary: &[String], callback: |*const *const libc::c_char| -> ()) {
-    fn helper_fn(ptrs: &mut Vec<*const libc::c_char>, ary: &[String], callback: |*const *const libc::c_char| -> ()) {
+fn str_array_to_char_pp(ary: &[String], callback: fn(*const *const libc::c_char) -> ()) {
+    fn helper_fn(ptrs: &mut Vec<*const libc::c_char>, ary: &[String], callback: fn(*const *const libc::c_char) -> ()) {
         match ary {
             [] => {
                 ptrs.push(ptr::null());
